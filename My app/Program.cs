c0cc230f3d2b80cda72e8adfce95e6d871aafcc0
Games <Games@DESKTOP-2GPRKIU>
2023-03-12 15:17:37 +0700

    Добавьте файлы проекта.

---
+++ My app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+    public class Programm
+    {
+        public static void Main()
+        {
+            Console.Write("Введите свое имя ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+        }
+
+           
+    }
