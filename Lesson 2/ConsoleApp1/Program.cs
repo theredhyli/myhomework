610dfa3075d8989599269ac7ea977f354ac5a656
Anna Bel <75969819+theredhyli@users.noreply.github.com>
2023-03-20 11:25:49 +0700

    Add files via upload

---
+++ Lesson 2/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Immutable;
+using System.ComponentModel.Design;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.Write($"Введите количество элементов массива:\t");
+        int elem;
+        bool result = int.TryParse(Console.ReadLine(), out elem);
+        if (result != false)
+        {
+            if (elem <= 1)
+            {
+                Console.WriteLine($"ведите значение больше 1");
+                return;
+            }
+
+            int[] array = new int[elem];
+
+            for (int i = 0; i < elem; i++)
+            {
+                Console.Write($"Введите {i + 1} значение элемента массива\t");
+                try
+                {
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Недопустимое значение");
+                    return;
+                }
+
+            }
+            int max = array[0];
+            int secondmax = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    secondmax = max;
+                    max = array[i];
+
+                }
+                else
+                      if (array[i] > secondmax && array[i] != max)
+                {
+                    secondmax = array[i];
+                }
+            }
+
+            if (secondmax != 0)
+            {
+                Console.WriteLine($"Второй максимальный элемент: {secondmax}");
+            }
+            else
+            {
+                Console.WriteLine("Второго максимального элемента не существует");
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("Недопустимо вводить буквы \nlol"); 
+            return;
+        }
+    }
+}
